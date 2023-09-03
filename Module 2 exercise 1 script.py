56c5bd477e52cb123bf1a1e1f7e4b506953d447b
EliC3333 <143453248+EliC3333@users.noreply.github.com>
2023-09-03 16:25:17 -0400

    Delete Module 2 exercise 1 script.py

--- Module 2 exercise 1 script.py
+++
@@ -1,9 +0,0 @@
-userinput = input("Enter a String: ")
-#Created an empty string
-reversed = ""
-#loops through each character 
-for char in userinput:
-    #adds each character in the empty string
-    reversed = char + reversed
-    #prints
-print(reversed)
